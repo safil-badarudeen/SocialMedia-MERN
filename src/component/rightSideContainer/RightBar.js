5ceccc1d3e936ca3cf1f1c3c4e431463eb65d968
safil badarudeen <safilbadarudeen@gmail.com>
2022-12-31 21:34:51 +0530

    partialy completed rightBar design

--- src/component/rightSideContainer/RightBar.js
+++ src/component/rightSideContainer/RightBar.js
@@ -1,10 +1,98 @@
-import React from 'react'
-import './rightBar.css'
+import React from "react";
+
+import AdImage from "../images/adimage.webp";
+
+import "./rightBar.css";
 
 function RightBar() {
   return (
-    <div className='RightbarContainer'>rightBar</div>
-  )
+    <div className="Rightbar">
+      <div className="RightContainer">
+        <div className="AdsContainer">
+          <div>
+            <img src={`${AdImage}`} className="AdsImage" alt=""></img>
+          </div>
+          <div className>
+            <p
+              style={{
+                textAlign: "start",
+                marginLeft: "10px",
+                marginTop: "-40px",
+              }}
+            >
+              buy udemy course
+            </p>
+            <p
+              style={{
+                textAlign: "start",
+                marginLeft: "10px",
+                fontSize: "12px",
+                marginTop: "-20px",
+              }}
+            >
+              offer will end soon
+            </p>
+          </div>
+        </div>
+        <div className="AdsContainer">
+          <div>
+            <img src={`${AdImage}`} className="AdsImage" alt=""></img>
+          </div>
+          <div className>
+            <p
+              style={{
+                textAlign: "start",
+                marginLeft: "10px",
+                marginTop: "-40px",
+              }}
+            >
+              buy udemy course
+            </p>
+            <p
+              style={{
+                textAlign: "start",
+                marginLeft: "10px",
+                fontSize: "12px",
+                marginTop: "-20px",
+              }}
+            >
+              offer will end soon
+            </p>
+          </div>
+        </div>
+        <div className="AdsContainer">
+          <div>
+            <img src={`${AdImage}`} className="AdsImage" alt=""></img>
+          </div>
+          <div className>
+            <p
+              style={{
+                textAlign: "start",
+                marginLeft: "10px",
+                marginTop: "-40px",
+              }}
+            >
+              buy udemy course
+            </p>
+            <p
+              style={{
+                textAlign: "start",
+                marginLeft: "10px",
+                fontSize: "12px",
+                marginTop: "-20px",
+              }}
+            >
+              offer will end soon
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="RightContainer2">
+        
+        
+      </div>
+    </div>
+  );
 }
 
-export default RightBar
+export default RightBar;
