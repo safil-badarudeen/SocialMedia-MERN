fe629338365d39b05582320c70e41443a489ae9e
safil badarudeen <safilbadarudeen@gmail.com>
2023-01-06 11:05:30 +0530

    added react router 6 added Link in navbar , to profile when clicking on user name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home/Home';
-import Profile from './pages/Profile/Profile';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home/> */}
-      <Profile/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import './Navbar.css'
-import search from '../images/search.png';
-import Notification from '../images/notificationIcon.png'
-import ProfilePic from '../images/profilePic.jpg'
-import Message from '../images/Message.png'
-
+import React from "react";
+import "./Navbar.css";
+import search from "../images/search.png";
+import Notification from "../images/notificationIcon.png";
+import ProfilePic from "../images/profilePic.jpg";
+import Message from "../images/Message.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="MainNavbar">
-        <div className="LogoContainer">
-                 <p>Social</p>
+      <div className="LogoContainer">
+        <p>Social</p>
+      </div>
+      <div>
+        <div className="SearchInputContainer">
+          <img src={`${search}`} className="SearchIcon" alt=""></img>
+          <input
+            type="text"
+            className="SearchInput"
+            placeholder="search your friends"
+            name=""
+            id=""
+          ></input>
         </div>
-        <div>
-           <div className="SearchInputContainer">
-              <img src={`${search}`} className="SearchIcon" alt=""></img>
-              <input type="text" className="SearchInput" placeholder="search your friends" name="" id=""></input>
-           </div>
-
-        </div>
-        <div className="IconsContainer">
-             <img src={`${Notification}`} className="IconsNav" alt="" ></img>
-             <img src={`${Message}`} className="IconsNav"  alt=''></img>
-              <div style={{display:'flex' , alignItems:'center'}} >
-                  <img src={`${ProfilePic}`} className="ProfileImage"  alt=""></img>
-                   <p style={{marginLeft:'10px'}}>Safil</p>
-              </div>
-        </div>
+      </div>
+      <div className="IconsContainer">
+        <img src={`${Notification}`} className="IconsNav" alt=""></img>
+        <img src={`${Message}`} className="IconsNav" alt=""></img>
+        <Link to="/profile/88888">
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={`${ProfilePic}`} className="ProfileImage" alt=""></img>
+            <p style={{ marginLeft: "10px" }}>Safil</p>
+          </div>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
