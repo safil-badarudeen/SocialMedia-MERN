f9348dba9a1169282eb44f60ea95d25c7bb57545
safil badarudeen <safilbadarudeen@gmail.com>
2023-01-07 17:28:10 +0530

    added backend update post route

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,5 +1,5 @@
 const Post = require("../models/Post");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const createPost = async (req, res) => {
   let { title, image, video } = req.body;
@@ -9,26 +9,40 @@ const createPost = async (req, res) => {
     image,
     video,
     user: req.user.id,
-  })
+  });
 
-
-  await newPost.save()
-  res.status(200).json({newPost , totalPost:newPost.length});
+  await newPost.save();
+  res.status(200).json({ newPost, totalPost: newPost.length });
 };
 
 //////////////////////////
 
-const getMyPost = async (req,res)=>{
-    const {id} = req.user
+const getMyPost = async (req, res) => {
+  const { id } = req.user;
 
-    const mypost = await Post.findOne({user : id})
+  const mypost = await Post.find({ user: id });
 
-    if(!mypost){
-       return res.status(400).json({msg:'You dont have any post'})
-    }
-    res.status(200).json({yourPost:mypost })
-}
+  if (!mypost) {
+    return res.status(400).json({ msg: "You dont have any post" });
+  }
+  res.status(200).json({ yourPost: mypost, totalPost: mypost.length });
+};
 
 ////////////////////////
+//update post
+////////
 
-module.exports = { createPost , getMyPost };
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  let post = await Post.findById(id);
+
+  if (!post) {
+    return res.status(400).json("No post found on this id");
+  }
+
+  post = await Post.findByIdAndUpdate(id, { $set: req.body });
+  let updatePost= await post.save()
+  res.status(200).json(updatePost)
+};
+
+module.exports = { createPost, getMyPost, updatePost };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {createPost , getMyPost} = require('../controllers/postController');
+const {createPost , getMyPost , updatePost} = require('../controllers/postController');
 const {verifyToken} = require('../utils/verifyToken')
 
 
 router.post('/user/createPost',verifyToken,createPost)
 router.get('/user/getMyPost',verifyToken,getMyPost)
+router.put('/user/updatePost/:id',verifyToken,updatePost)
 
 
 module.exports = router;
