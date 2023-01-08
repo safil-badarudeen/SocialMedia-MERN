7f664b22736a7c86ada71cd1e1e90de108481295
safil badarudeen <safilbadarudeen@gmail.com>
2023-01-08 13:09:09 +0530

    addded comment route and tested  to be ok in backend

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -51,7 +51,7 @@ const updatePost = async (req, res) => {
 };
 
 ////////////////
-//following
+//following  // this route can be added into another folder for good readable structure i think. make a look
 ///////////////
 
 const following = async (req, res) => {
@@ -86,10 +86,54 @@ const followingPost = async (req, res) => {
   res.status(200).json(posts);
 };
 
+//like
+
+const like = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post.like.includes(req.body.user)) {
+    await post.updateOne({ $push: { like: req.body.user } });
+    return res.status(200).json("post has been liked ");
+  } else {
+    await post.updateOne({ $pull: { like: req.body.user } });
+    return res.status(200).json("like removed");
+  }
+};
+
+//dislike
+
+const dislike = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post.dislike.includes(req.body.user)) {
+    await post.updateOne({ $push: { dislike: req.body.user } });
+    return res.status(200).json("post has been disliked ");
+  } else {
+    await post.updateOne({ $pull: { dislike: req.body.user } });
+    return res.status(200).json("dislike removed");
+  }
+};
+
+const comment = async (req,res)=>{
+  const { postId, comment} = req.body
+  const comments = {
+    user : req.user.id,
+    username:req.user.username,
+    comment,
+  }
+  const post = await Post.findById(postId)
+  post.comments.push(comments),
+  await post.save();
+  res.status(200).json(post)
+}
+
+
+
 module.exports = {
   createPost,
   getMyPost,
   updatePost,
   following,
   followingPost,
+  like,
+  dislike,
+  comment
 };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const {createPost , getMyPost , updatePost,following,followingPost} = require('../controllers/postController');
-const {verifyToken} = require('../utils/verifyToken')
+const {
+  createPost,
+  getMyPost,
+  updatePost,
+  following,
+  followingPost,
+  like,
+  dislike,
+  comment
+} = require("../controllers/postController");
+const { verifyToken } = require("../utils/verifyToken");
 
+router.post("/user/createPost", verifyToken, createPost);
+router.get("/user/getMyPost", verifyToken, getMyPost);
 
-router.post('/user/createPost',verifyToken,createPost)
-router.get('/user/getMyPost',verifyToken,getMyPost)
-router.put('/user/updatePost/:id',verifyToken,updatePost)
+//like and dislike
+router.put("/:id/like", verifyToken, like, like);
+router.put("/:id/dislike", verifyToken, dislike);
 
-router.put('/user/following/:id',verifyToken,following)
+//comment 
+router .put ('/user/comment',verifyToken, comment)
+
+router.put("/user/updatePost/:id", verifyToken, updatePost);
+
+router.put("/user/following/:id", verifyToken, following);
 
 //fetch post from following
-router.get('/user/followingPost/:id',verifyToken,followingPost)
+router.get("/user/followingPost/:id", verifyToken, followingPost);
 
 module.exports = router;
